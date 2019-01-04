a52e8c3ad4bb9f13ccc6925d2cd501dd302140a9
Jonathan S. Katz <jonathan.katz@excoventures.com>
2019-01-04 16:32:06 -0500

    Fix for pattern matching for detecting 9.x docs.
    Credit: Andrew Crenshaw <andrew.crenshaw@crunchydata.com>

--- etl/postgresql/postgresql_var.sh
+++ etl/postgresql/postgresql_var.sh
@@ -21,7 +21,7 @@ CONTENT="${DST}/content"
 BUILD_ROOT="${DIR}/build"
 BUILD="${BUILD_ROOT}/${REPO}_${POSTGRESQL_VERSION}"
 TMP="/tmp/${REPO}_${POSTGRESQL_VERSION}"
-if [ "$POSTGRESQL_VERSION" == '9*' ]; then
+if [[ "$POSTGRESQL_VERSION" == "9"* ]]; then
     REPO_MAJOR=$(echo ${POSTGRESQL_VERSION} | sed -r 's/([^_]*)_([^_]*).*/\1\2/')
 else
     REPO_MAJOR=$(echo ${POSTGRESQL_VERSION} | sed -r 's/([^_]*).*/\1/')
